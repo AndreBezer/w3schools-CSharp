e39c357ad69e3298fff929bf05fbe5b2926c8b10
André Luis <andrelus475@gmail.com>
2024-09-18 22:00:08 -0300

    Create entendendo_Diferencas.cs
    Entendendo a Diferença entre comando Write e WriteLine em C#

---
+++ entendendo_Diferencas.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Entendendo_Diferenca_Entre_WriteLine
+{
+
+    // Entendendo a diferenca entre
+    // Comandos WriteLine
+    // E comandos Write
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //WriteLine
+            Console.WriteLine("Este é um ");
+            Console.WriteLine("Comando WriteLine ");
+            Console.WriteLine("Legal...");
+
+            //Write
+            Console.Write("Este é um ");
+            Console.Write("Comando Write, ");
+            Console.Write("Show ");
+        }
+    }
+}
